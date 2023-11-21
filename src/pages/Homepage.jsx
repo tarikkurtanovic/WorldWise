89781687420da7942fddd1f023460a5b3469e9b2
tarikkurtanovic <tarik2371@gmail.com>
2023-11-21 17:37:44 +0100

    Building the pages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,18 +1,26 @@
-import { NavLink } from "react-router-dom";
+import styles from "./Homepage.module.css";
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
-import AppNav from "../components/AppNav";
 
-function Homepage() {
-  <PageNav />;
-
+export default function Homepage() {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNav />
-      <AppNav />
-      <h1>Worldwise</h1>
-      <NavLink to="/app">Go to the app</NavLink>
-    </div>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/app" className="cta">
+          Start tracking now
+        </Link>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
