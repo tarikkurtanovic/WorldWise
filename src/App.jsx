cc458eaf7fcf284767d86408be3976d6e9b3c1d6
tarikkurtanovic <tarik2371@gmail.com>
2023-11-24 18:19:47 +0100

    Implementing the Cities list

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 function App() {
-  const [cities, setCities] = useState({});
+  const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
@@ -37,8 +37,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
